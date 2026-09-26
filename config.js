6c23d19943c2a30d2f72f4f881cb5980bade778b
Vizuka <anishkarki745@gmail.com>
2026-09-26 09:31:19 +0545

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 /* PUBLIC CONFIG ONLY. Never put a Supabase secret key here. */
 window.VIZUKA_CONFIG = {
-  SUPABASE_URL: "https://tqikvafklxcrnqcvhyoz.supabase.com",
+  SUPABASE_URL: "https://tqikvafklxcrnqcvhyoz.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_9uCJE7GZOvxi0Cp8H3-A7g_k9fCo_CF"
 };
